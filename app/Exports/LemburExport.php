a04cc8cc7500ac1c28789c7a6c576b614ba297e3
AnggiPurba <dwipurba77@gmail.com>
2024-08-17 16:14:16 +0700

    Print Berhasil, Excel ada bug

--- app/Exports/LemburExport.php
+++ app/Exports/LemburExport.php
@@ -4,10 +4,12 @@ namespace App\Exports;
 
 use App\Models\Lembur;
 use App\Models\Karyawan;
+use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class LemburExport implements FromView
+class LemburExport implements FromView, WithStyles
 {
     protected $startDate;
     protected $endDate;
@@ -22,13 +24,17 @@ class LemburExport implements FromView
 
     public function view(): View
     {
-        // Fetch the Lembur data with the joined Karyawan table to get jabatan
-        $lemburData = Lembur::select('lembur.*', 'karyawan.jabatan') // Select all fields from lembur and jabatan from karyawan
+        $lemburData = Lembur::select('lembur.*', 'karyawan.jabatan', 'karyawan.gaji')
                             ->join('karyawan', 'lembur.nama_lengkap', '=', 'karyawan.nama_karyawan')
                             ->whereBetween('lembur.tanggal_lembur', [$this->startDate, $this->endDate])
                             ->where('lembur.nama_lengkap', 'like', '%' . $this->namaLengkap . '%')
                             ->get();
-    
+
+        foreach ($lemburData as $lembur) {
+            $gaji = (float) ($lembur->gaji ?? 0);
+            $lembur->upahLemburPerJam = $gaji > 0 ? round($gaji / 173, 2) : 0;
+        }
+
         return view('excelPerhitunganLembur', [
             'lemburData' => $lemburData,
             'startDate' => $this->startDate,
@@ -36,5 +42,35 @@ class LemburExport implements FromView
             'namaLengkap' => $this->namaLengkap
         ]);
     }
-    
+
+    public function styles(Worksheet $sheet)
+{
+    // Apply Book Antiqua font and size 9 to the entire sheet
+    $sheet->getStyle($sheet->calculateWorksheetDimension())
+          ->applyFromArray([
+              'font' => [
+                  'name' => 'Book Antiqua',
+                  'size' => 9,  // Set font size to 9
+              ],
+          ]);
+
+    // Apply alignment and borders only to the table range
+    $tableRange = 'A7:Q' . ($sheet->getHighestRow()); // Adjust range as needed
+    $sheet->getStyle($tableRange)->applyFromArray([
+        'alignment' => [
+            'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+            'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+        ],
+        'borders' => [
+            'allBorders' => [
+                'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+            ],
+        ],
+        
+    ]);
+
+    return $sheet;
+}
+
+
 }
--- resources/views/excelPerhitunganLembur.blade.php
+++ resources/views/excelPerhitunganLembur.blade.php
@@ -10,39 +10,45 @@
         $nama = $namaLengkap;
         $jabatan = $firstLembur->jabatan ?? 'N/A';
         $gaji = (float) ($firstLembur->gaji ?? 0);
-        $upahLemburPerJam = $gaji > 0 ? round($gaji / 173, 2) : 0;
     @endphp
-
-    <label>Nama</label>
-    <span class="value">: {{ $nama }}</span><br>
-
-    <label>Jabatan</label>
-    <span class="value">: {{ $jabatan }}</span><br>
-
-    @if($startDate && $endDate)
-    @php
-        $formattedStartDate = \Carbon\Carbon::parse($startDate)->locale('id')->translatedFormat('d F Y');
+<table class="new-table">>
+        <thead>
+            <tr>
+                <th colspan="3">Nama</th>
+                <th>:</th>
+                <th colspan="3">{{ $nama }}</th>
+            </tr>
+            <tr>
+                <th colspan="3">Jabatan</th>
+                <th>:</th>
+                <th colspan="3">{{ $jabatan }}</th>
+            </tr>
+            @php
+        $formattedStartDate = \Carbon\Carbon::parse($startDate)->locale('id')->translatedFormat('d F ');
         $formattedEndDate = \Carbon\Carbon::parse($endDate)->locale('id')->translatedFormat('d F Y');
     @endphp
-    <label>Periode</label>
-    <span class="value">: {{ $formattedStartDate }} - {{ $formattedEndDate }}</span><br>
+            <tr>
+                <th colspan="3">Periode</th>
+                <th>:</th>
+                <th colspan="3">{{ $formattedStartDate }} - {{ $formattedEndDate }}</th>
+            </tr>
+            <tr>
+                <th colspan="3">Gaji Pokok</th>
+                <th>:</th>
+                <th colspan="3">{{ number_format($gaji, 0, ',', '.') }}</th>
+            </tr>
+            <tr>
+                <th colspan="3">Upah lembur per  jam</th>
+                <th>:</th>
+                <th colspan="3">{{ number_format($gaji, 0, ',', '.') }} / 173 = {{ number_format($firstLembur->upahLemburPerJam, 0, ',', '.') }}</th>
+            </tr>
+        </thead>
+    </table>
     @else
-    <label>Periode</label>
-    <span class="value">: Tidak ada periode yang ditentukan</span><br>
-    @endif
-
-    <label>Gaji Pokok</label>
-    <span class="value">: {{ number_format($gaji, 0, ',', '.') }}</span><br>
-
-    <label>Upah lembur per jam</label>
-    <span class="value">: {{ number_format($gaji, 0, ',', '.') }} / 173 = {{ number_format($upahLemburPerJam, 2, ',', '.') }}</span>
-
-@else
     <p>No records found for the given criteria.</p>
 @endif
 
-    <div class="spacer"></div>
-    <table style="border: 1px solid;">
+    <table>
     <thead>
         <tr>
             <th rowspan="2">No</th>
@@ -54,7 +60,7 @@
             <th rowspan="2">Keterangan</th>
         </tr>
         <tr>
-            <th>In</th>
+            <th style="border: 1px solid #dddddd; padding: 5px;">In</th>
             <th>Out</th>
             <th>Total</th>
             <th>Jam I</th>
@@ -69,75 +75,76 @@
         </tr>
     </thead>
     <tbody>
-        @php
-            // Initialize totals
-            $totalJamKerjaLembur = 0;
-            $totalJamI = 0;
-            $totalJamI1_5 = 0;
-            $totalJamII = 0;
-            $totalJamII2 = 0;
-            $totalJamIII = 0;
-            $totalJamIII3 = 0;
-            $totalJamIV = 0;
-            $totalJamIV4 = 0;
-            $totalTotalJamLembur = 0;
-            $totalUpahLembur = 0;
-        @endphp
+    @php
+        // Initialize totals
+        $totalJamKerjaLembur = 0;
+        $totalJamI = 0;
+        $totalJamI1_5 = 0;
+        $totalJamII = 0;
+        $totalJamII2 = 0;
+        $totalJamIII = 0;
+        $totalJamIII3 = 0;
+        $totalJamIV = 0;
+        $totalJamIV4 = 0;
+        $totalTotalJamLembur = 0;
+        $totalUpahLembur = 0;
+    @endphp
 
-        @foreach ($lemburData as $lembur)
-        <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ \Carbon\Carbon::parse($lembur->tanggal_lembur)->locale('id')->translatedFormat('l') }}</td>
-            <td>{{ \Carbon\Carbon::parse($lembur->tanggal_lembur)->locale('id')->translatedFormat('d F Y') }}</td>
-            <td>{{ \Carbon\Carbon::parse($lembur->jam_masuk)->format('H:i') }}</td>
-            <td>{{ \Carbon\Carbon::parse($lembur->jam_keluar)->format('H:i') }}</td>
-            <td>{{ $lembur['jam_kerja_lembur'] ?? 0 }}</td>
-            <td>{{ $lembur['jam_i']  ?? 0}}</td>
-            <td>{{ $lembur['jam_i'] * 1.5 }}</td>
-            <td>{{ $lembur['jam_ii'] ?? 0 }}</td>
-            <td>{{ $lembur['jam_ii'] * 2 }}</td>
-            <td>{{ $lembur['jam_iii'] ?? 0 }}</td>
-            <td>{{ $lembur['jam_iii'] * 3 }}</td>
-            <td>{{ $lembur['jam_iv'] ?? 0 }}</td>
-            <td>{{ $lembur['jam_iv'] * 4 }}</td>
-            <td>{{ $lembur['total_jam_lembur'] }}</td>
-            <td>{{ $lembur['upah_lembur'] }}</td>
-            <td>{{ $lembur['keterangan'] }}</td>
-            
-            @php
-                // Accumulate totals
-                $totalJamKerjaLembur += $lembur['jam_kerja_lembur'];
-                $totalJamI += $lembur['jam_i'];
-                $totalJamI1_5 += $lembur['jam_i'] * 1.5;
-                $totalJamII += $lembur['jam_ii'];
-                $totalJamII2 += $lembur['jam_ii'] * 2;
-                $totalJamIII += $lembur['jam_iii'];
-                $totalJamIII3 += $lembur['jam_iii'] * 3;
-                $totalJamIV += $lembur['jam_iv'];
-                $totalJamIV4 += $lembur['jam_iv'] * 4;
-                $totalTotalJamLembur += $lembur['total_jam_lembur'];
-                $totalUpahLembur += $lembur['upah_lembur'];
-            @endphp
-        </tr>
-        @endforeach
-    </tbody>
-    <tfoot>
-        <tr>
+    @foreach ($lemburData as $lembur)
+    <tr>
+        <td>{{ $loop->iteration }}</td>
+        <td>{{ \Carbon\Carbon::parse($lembur->tanggal_lembur)->locale('id')->translatedFormat('l') }}</td>
+        <td>{{ \Carbon\Carbon::parse($lembur->tanggal_lembur)->locale('id')->translatedFormat('d F Y') }}</td>
+        <td>{{ \Carbon\Carbon::parse($lembur->jam_masuk)->format('H:i') }}</td>
+        <td>{{ \Carbon\Carbon::parse($lembur->jam_keluar)->format('H:i') }}</td>
+        <td>{{ number_format($lembur['jam_kerja_lembur'] ?? 0, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['jam_i'] ?? 0, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['jam_i'] * 1.5, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['jam_ii'] ?? 0, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['jam_ii'] * 2, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['jam_iii'] ?? 0, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['jam_iii'] * 3, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['jam_iv'] ?? 0, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['jam_iv'] * 4, 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['total_jam_lembur'], 1, ',', '.') }}</td>
+        <td>{{ number_format($lembur['upah_lembur'], 0, ',', '.') }}</td>
+        <td>{{ $lembur['keterangan'] }}</td>
+        
+        @php
+            // Accumulate totals
+            $totalJamKerjaLembur += $lembur['jam_kerja_lembur'];
+            $totalJamI += $lembur['jam_i'];
+            $totalJamI1_5 += $lembur['jam_i'] * 1.5;
+            $totalJamII += $lembur['jam_ii'];
+            $totalJamII2 += $lembur['jam_ii'] * 2;
+            $totalJamIII += $lembur['jam_iii'];
+            $totalJamIII3 += $lembur['jam_iii'] * 3;
+            $totalJamIV += $lembur['jam_iv'];
+            $totalJamIV4 += $lembur['jam_iv'] * 4;
+            $totalTotalJamLembur += $lembur['total_jam_lembur'];
+            $totalUpahLembur += $lembur['upah_lembur'];
+        @endphp
+    </tr>
+    @endforeach
+</tbody>
+<tfoot>
+    <tr>
         <th colspan="5">Total</th>
-            <td>{{ $totalJamKerjaLembur }}</td>
-            <td>{{ $totalJamI }}</td>
-            <td>{{ $totalJamI1_5 }}</td>
-            <td>{{ $totalJamII }}</td>
-            <td>{{ $totalJamII2 }}</td>
-            <td>{{ $totalJamIII }}</td>
-            <td>{{ $totalJamIII3 }}</td>
-            <td>{{ $totalJamIV }}</td>
-            <td>{{ $totalJamIV4 }}</td>
-            <td>{{ $totalTotalJamLembur }}</td>
-            <td>{{  $totalUpahLembur }}</td>
-            <td></td>
-        </tr>
-    </tfoot>
+        <td>{{ number_format($totalJamKerjaLembur, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalJamI, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalJamI1_5, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalJamII, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalJamII2, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalJamIII, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalJamIII3, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalJamIV, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalJamIV4, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalTotalJamLembur, 1, ',', '.') }}</td>
+        <td>{{ number_format($totalUpahLembur, 0, ',', '.') }}</td>
+        <td></td>
+    </tr>
+</tfoot>
+
 </table>
 
 
